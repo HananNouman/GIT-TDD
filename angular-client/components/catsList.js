2f4d0590057980e8518e4ef6e7f395d5bf078bf8
montheramer <monther.amer48@gmail.com>
2018-08-15 16:42:01 +0300

    If applied, this commit will addCat/getcat from frontend

--- angular-client/components/catsList.js
+++ angular-client/components/catsList.js
@@ -5,15 +5,42 @@ app.component('cats', {
 });
 app.controller('catsCtrl', function ($scope, $http,$window){
 var getCats = function() {
-	// TODO: Your code here
+	$http({
+  method: 'GET',
+  url: '/cats'
+}).then(function successCallback(response) {
+	$scope.cats=response.data;
+    
+  }, function errorCallback(response) {
+    console.log(response);
+  });
+	
 
 };
 getCats();
 
-$scope.addCats=function() {
+$scope.addCats=function(){
+	$http({
+  method: 'POST',
+  url: '/cats',
+  headers: {
+   'Content-Type': "application/JSON"
+ },
+ data: {catName:$scope.catName,
+  ownerEmail:$scope.ownerEmail,
+  imageUrl:$scope.imageUrl,
+  adoptionMessage:$scope.adoptionMessage}
+}).then(function successCallback(response) {
+	console.log(response);
+    
+  }, function errorCallback(response) {
+    console.log(response);
+  });
+	
+
+};
 // TODO: Your code here
 
 
-}
 
 });
